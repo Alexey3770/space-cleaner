a0f5a63295528127cfec997e3ba613d4f1492cbf
Flamingo-37 <flamingo37rus@gmail.com>
2019-10-19 14:56:27 +0300

    no message

--- core/src/com/dethreeca/space_cleaner/state/GameMenu.java
+++ core/src/com/dethreeca/space_cleaner/state/GameMenu.java
@@ -4,29 +4,50 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.dethreeca.space_cleaner.SpaceCleaner;
 
 public class GameMenu extends State {
-    OrthographicCamera camera;
     SpriteBatch batch;
     Rectangle buttonRectangle;
-    Texture buttonTexture;
+    TextureRegion buttonTexture;
+    Rectangle backGroundRectangle;
+    TextureRegion backGroundTexture;
+
     public GameMenu(GameStateManager gsm){
         super(gsm);
         batch = new SpriteBatch();
         buttonRectangle = new Rectangle();
-        buttonRectangle.width = 64;
-        buttonRectangle.height = 64;
+        buttonRectangle.width = SpaceCleaner.WIDTH %600;
+        buttonRectangle.height = SpaceCleaner.HEIGTH%600;
         buttonRectangle.x = SpaceCleaner.WIDTH/2-buttonRectangle.width/2;
         buttonRectangle.y = SpaceCleaner.HEIGTH/2-buttonRectangle.height/2;
-        buttonTexture = new Texture("button1.png");
+        buttonTexture = new TextureRegion(new Texture("MenuButton.png"));
+
+        backGroundRectangle = new Rectangle();
+        backGroundRectangle.width = SpaceCleaner.WIDTH;
+        backGroundRectangle.height = SpaceCleaner.HEIGTH;
+        backGroundRectangle.x=0;
+        backGroundRectangle.y=0;
+        backGroundTexture = new TextureRegion(new Texture("bg.png"));
+
 
     }
 
     @Override
     protected void handleInput() {
         if(Gdx.input.justTouched()){
+            float pressMenuButton = buttonRectangle.height/3 ;
+            if(Gdx.input.getY()>buttonRectangle.y && Gdx.input.getY()<buttonRectangle.y+pressMenuButton){
+                gsm.push(new GamePlay(gsm));
+            }
+            if(Gdx.input.getY()>buttonRectangle.y+pressMenuButton && Gdx.input.getY()<buttonRectangle.y+2*pressMenuButton){
+                gsm.push(new GamePlay(gsm));
+            }
+            if(Gdx.input.getY()>buttonRectangle.y+2*pressMenuButton && Gdx.input.getY()<buttonRectangle.y+3*pressMenuButton){
+                gsm.push(new GamePlay(gsm));
+            }
             gsm.set(new GamePlay(gsm));
         }
     }
@@ -38,7 +59,6 @@ public class GameMenu extends State {
     @Override
     public void update(float dt) {
         super.update(dt);
-        camera.update();
     }
 
     @Override
@@ -46,8 +66,11 @@ public class GameMenu extends State {
     }
 
     @Override
-    public void renderStatic(SpriteBatch staticBatch) {
-        super.renderStatic(staticBatch);
+    public void renderStatic(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(backGroundTexture, backGroundRectangle.x,backGroundRectangle.y,backGroundRectangle.width,backGroundRectangle.height);
+        sb.draw(buttonTexture, buttonRectangle.x,buttonRectangle.y, 500, 500);
+        sb.end();
     }
 
     @Override
---
+++ core/src/com/dethreeca/space_cleaner/state/GameSettings.java
@@ -0,0 +1,19 @@
+package com.dethreeca.space_cleaner.state;
+
+
+public class  GameSettings extends State {
+
+    public GameSettings(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleAccelerometer() {
+
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+}
